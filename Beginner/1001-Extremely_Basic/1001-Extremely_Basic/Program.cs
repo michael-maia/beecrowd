779896667258ad22a9cae0022339a2ff1c7c2163
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-07 19:42:00 -0200

    Correct answer to the 1001 - Extremely Basic question on the Beginner category.

---
+++ Beginner/1001-Extremely_Basic/1001-Extremely_Basic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1001_Extremely_Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A,B,X;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            X = A + B;
+            Console.WriteLine("X = {0}", X);
+            Console.ReadKey();
+        }
+    }
+}
