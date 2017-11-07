52d167def08c98574d4d988d2d7ff01376f612bb
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-07 20:21:45 -0200

    Answer to question 1004 - Simple Product

---
+++ Beginner/1004-Simple_Product/1004-Simple_Product/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1004_Simple_Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, PROD;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            PROD = A * B;
+            Console.WriteLine("PROD = {0}", PROD);
+            Console.ReadKey();
+        }
+    }
+}
