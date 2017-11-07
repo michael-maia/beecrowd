99dc8ce4f6b56aaddf6e26c9a755d5ad305aa5c7
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-07 20:18:21 -0200

    The answer to question 1003 - Simple Sum

---
+++ Beginner/1003-Simple_Sum/1003-Simple_Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1003_Simple_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A,B,SOMA;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            SOMA = A + B;
+            Console.WriteLine("SOMA = {0}", SOMA);
+            Console.ReadKey();
+        }
+    }
+}
