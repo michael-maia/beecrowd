8db3772f582ea8b51a2d327c5edfeffd2291b133
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-07 20:36:24 -0200

    Answer to question 1006 - Average 2

---
+++ Beginner/1006-Average2/1006-Average2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1006_Average2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, MEDIA;
+            A = double.Parse(Console.ReadLine());
+            B = double.Parse(Console.ReadLine());
+            C = double.Parse(Console.ReadLine());
+            MEDIA = (A * 2 + B * 3 + C * 5) / 10.0;
+            Console.WriteLine("MEDIA = " + MEDIA.ToString("0.0"));
+            Console.ReadKey();
+        }
+    }
+}
