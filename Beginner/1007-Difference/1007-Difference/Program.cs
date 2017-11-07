eb0684ff2c6074ba8e6a1a3779b5a4b166c3b51d
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-07 20:40:41 -0200

    Answer to question 1007 - Difference

---
+++ Beginner/1007-Difference/1007-Difference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1007_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C, D, DIFERENCA;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+            DIFERENCA = (A * B - C * D);
+            Console.WriteLine("DIFERENCA = {0}", DIFERENCA);
+            Console.ReadKey();
+        }
+    }
+}
