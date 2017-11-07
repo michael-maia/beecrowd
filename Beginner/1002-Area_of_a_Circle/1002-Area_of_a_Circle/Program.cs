0a5e46be829cfe3fe5a148fb1c3ac34543f1f0b6
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-07 20:10:36 -0200

    This is the correct answer to the 1002 question in the Beginner category

---
+++ Beginner/1002-Area_of_a_Circle/1002-Area_of_a_Circle/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1002_Area_of_a_Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, pi, R;
+            pi = 3.14159;
+            R = double.Parse(Console.ReadLine());
+            A = pi * (R * R);
+            Console.WriteLine("A="+A.ToString("0.0000")); //This string format allow only 4 digits to decimal
+            Console.ReadKey();
+        }
+    }
+}
