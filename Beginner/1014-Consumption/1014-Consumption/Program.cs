a1b9b2c5b72e2297da6826388e2b8bfb56304ccf
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-08 19:38:31 -0200

    Answer to question 1014 - Consumption

---
+++ Beginner/1014-Consumption/1014-Consumption/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1014_Consumption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question: https://www.urionlinejudge.com.br/judge/en/problems/view/1014
+            int X;
+            double Y, average;
+            X = int.Parse(Console.ReadLine());
+            Y = double.Parse(Console.ReadLine());
+            average = X / Y;
+            Console.WriteLine(average.ToString("0.000") + " km/l");
+            Console.ReadKey();
+        }
+    }
+}
