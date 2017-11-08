cac7a2bf7e7a24015b65900b0880da679706448d
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-08 19:24:12 -0200

    Answer to question 1013 - The Greatest

---
+++ Beginner/1013-The_Greatest/1013-The_Greatest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1013_The_Greatest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question: https://www.urionlinejudge.com.br/judge/en/problems/view/1013
+            int A, B, C, greatest;
+            string[] values = Console.ReadLine().Split();
+            A = int.Parse(values[0]);
+            B = int.Parse(values[1]);
+            C = int.Parse(values[2]);
+            greatest = (A + B + Math.Abs(A - B)) / 2;
+            if (greatest > C)
+                Console.WriteLine("{0} eh o maior", greatest);
+            else
+                Console.WriteLine("{0} eh o maior", C);
+            Console.ReadKey();
+        }
+    }
+}
