4f4523ad9c588559d686c7ca6b7bbf2396f1f30a
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-08 18:15:53 -0200

    Answer to question 1009 - Salary with Bonus

---
+++ Beginner/1009-Salary_with_Bonus/1009-Salary_with_Bonus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1009_Salary_with_Bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double fixedSalary, saleTotal, finalSalary;
+            name = Console.ReadLine();
+            fixedSalary = double.Parse(Console.ReadLine());
+            saleTotal = double.Parse(Console.ReadLine());
+            finalSalary = fixedSalary + saleTotal * 0.15;
+            Console.WriteLine("TOTAL = R$ " + finalSalary.ToString("0.00"));
+            Console.ReadKey();
+        }
+    }
+}
