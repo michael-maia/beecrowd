7d66930096e24676ef88e9592377cabf0cab757a
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-08 17:59:56 -0200

    Answer to the question 1008 - Salary

---
+++ Beginner/1008-Salary/1008-Salary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1008_Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, hoursPerMonth;
+            double receivedPerHour,salary;
+            number = int.Parse(Console.ReadLine());
+            hoursPerMonth = int.Parse(Console.ReadLine());
+            receivedPerHour = double.Parse(Console.ReadLine());
+            salary = receivedPerHour * hoursPerMonth;
+            Console.WriteLine("NUMBER = " + number + "\nSALARY = U$ " + salary.ToString("0.00"));
+            Console.ReadKey();
+        }
+    }
+}
