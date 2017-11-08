9dbc882b6f8466ce3e21996e8e2f4e2e2eba9560
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-08 19:01:46 -0200

    Answer to the question 1011 - Sphere

---
+++ Beginner/1011-Sphere/1011-Sphere/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1011_Sphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question: https://www.urionlinejudge.com.br/judge/en/problems/view/1011
+            double R, volume;
+            R = double.Parse(Console.ReadLine());
+            volume = (4.0 / 3.0) * 3.14159 * (R * R * R);
+            Console.WriteLine("VOLUME = " + volume.ToString("0.000"));
+            Console.ReadKey();
+        }
+    }
+}
