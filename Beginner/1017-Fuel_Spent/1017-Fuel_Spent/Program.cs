6296f7be6a145074b47b019700ea4dee70bfc288
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-09 17:29:57 -0200

    Answer to question 1017 - Fuel Spent

---
+++ Beginner/1017-Fuel_Spent/1017-Fuel_Spent/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1017_Fuel_Spent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question: https://www.urionlinejudge.com.br/judge/en/problems/view/1017
+            int time = int.Parse(Console.ReadLine());
+            int averageSpeed = int.Parse(Console.ReadLine());
+            double averageFuel = 12.0; //Car fuel consumption (12 km/L)
+            int distance = averageSpeed * time;
+            double liters = distance / averageFuel; //We need at least one double precision variable in the division otherwise we will get a rounded value
+            Console.WriteLine(liters.ToString("0.000"));
+            Console.ReadKey();
+        }
+    }
+}
