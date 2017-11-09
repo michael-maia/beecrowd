a362d24e1fd3d21c8be97e940d158596f471315e
Michael Maia <michaelmdasilva@yahoo.com.br>
2017-11-09 21:15:30 -0200

    Answer to question 1019 - Time Conversion

---
+++ Beginner/1019-Time_Conversion/1019-Time_Conversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1019_Time_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Question: https://www.urionlinejudge.com.br/judge/en/problems/view/1019
+            int N = int.Parse(Console.ReadLine());
+            int hours = N / 3600;
+            N = N % 3600;
+            int minutes = N / 60;
+            N = N % 60;
+            Console.WriteLine("{0}:{1}:{2}", hours, minutes, N);
+            Console.ReadKey();
+        }
+    }
+}
