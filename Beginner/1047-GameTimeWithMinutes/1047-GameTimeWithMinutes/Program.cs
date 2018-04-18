d870266befa32ea0224bb7fd560607f0665625cb
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2018-04-18 14:56:31 -0300

    Update Program.cs
    Work in progress,
    
    I need to do something like h_var%60; 
    to discover the minutes remaining in the final result.

--- Beginner/1047-GameTimeWithMinutes/1047-GameTimeWithMinutes/Program.cs
+++ Beginner/1047-GameTimeWithMinutes/1047-GameTimeWithMinutes/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-
+using System;
 //Question : https://www.urionlinejudge.com.br/judge/en/problems/view/1047
 namespace _1047_GameTimeWithMinutes
 {
@@ -12,29 +11,17 @@ namespace _1047_GameTimeWithMinutes
             int m_start = int.Parse(values[1]);
             int h_final = int.Parse(values[2]);
             int m_final = int.Parse(values[3]);
-            int h_var = 0,m_var = 0;
+            int m_var = 0;
+            m_start = (h_start*60) + m_start;
+            m_final = (h_final*60) + m_final;
             do
             {
                 m_start++;
                 m_var++;
-                if (h_start == 24)
-                {
-                    h_start = 0;
-                }
-                else if (m_start == 60)
-                {
-                    m_start = 0;
-                    h_start++;
-                }
-                else if (m_var == 60)
-                {
-                    m_var = 0;
-                    h_var++;
-                }                
-                
-            } while (!(h_start == h_final && m_start == m_final));
-            Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", h_var, m_var);
-            Console.ReadKey();
+            } while (!(m_start == m_final));
+            h_var=m_var/60;
+            
+            Console.WriteLine("O JOGO DUROU {0} HORA(S) E {1} MINUTO(S)", h_var, m_var);            
         }
     }
 }
