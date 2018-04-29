fb2f0b6334853fa7be3cfe7ed6eacb32c7afe89b
Michael Maia <michaelmdasilva@yahoo.com.br>
2018-04-29 16:25:34 -0300

    Answer to the question 1043-Triangle

---
+++ Beginner/1043-Triangle/1043-Triangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        //Question : https://www.urionlinejudge.com.br/judge/en/problems/view/1043
+
+        string[] values = Console.ReadLine().Split(' ');
+        float A = float.Parse(values[0]);
+        float B = float.Parse(values[1]);
+        float C = float.Parse(values[2]);
+        float result;
+        bool state = false;
+        //condition to check if its a triangle or not
+        if (A < (B + C) && B < (A + C) && C < (A + B))
+        {
+            state = true;
+        }
+        //if state==false, calculate the area of a trapezium
+        if (state == false)
+        {
+            result = (A + B) * C / 2;
+            Console.WriteLine("Area = " + result.ToString("0.0"));
+        }
+        //if state==true, calculate the perimeter of the triangle
+        else
+        {
+            result = A + B + C;
+            Console.WriteLine("Perimetro = " + result.ToString("0.0"));
+        }
+    }
+}
