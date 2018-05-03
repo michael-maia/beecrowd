a5f000e177f8100780f8ea5866460059b4c81640
Michael Maia <michaelmdasilva@yahoo.com.br>
2018-05-03 18:25:50 -0300

    Answer to question 1046.

---
+++ Beginner/1046-GameTime/1046-GameTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] values = Console.ReadLine().Split(' ');
+        int horai = int.Parse(values[0]);
+        int horaf = int.Parse(values[1]);
+        int contador = 0;
+
+        do
+        {
+            if (horai == 24)
+            {
+                horai = 0;
+            }
+            else
+            {
+                horai++;
+                contador++;
+            }
+        }
+        while (horai != horaf);
+
+        Console.WriteLine("O JOGO DUROU {0} HORA(S)", contador);
+    }
+}
