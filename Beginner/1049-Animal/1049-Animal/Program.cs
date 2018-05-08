d8cfe13946512964202167e9e721d21a561d2562
Michael Maia <michaelmdasilva@yahoo.com.br>
2018-05-08 18:20:59 -0300

    Answer to question 1049 - Animal.

---
+++ Beginner/1049-Animal/1049-Animal/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string word1 = Console.ReadLine();
+        string word2 = Console.ReadLine();
+        string word3 = Console.ReadLine();
+
+        if (word1.ToLower() == "vertebrado")
+        {
+            if (word2.ToLower() == "ave")
+            {
+                if (word3.ToLower() == "carnivoro")
+                {
+                    Console.WriteLine("aguia");
+                }
+                else //onivoro
+                {
+                    Console.WriteLine("pomba");
+                }
+            }
+            else //mamifero
+            {
+                if (word3.ToLower() == "onivoro")
+                {
+                    Console.WriteLine("homem");
+                }
+                else //herbivoro
+                {
+                    Console.WriteLine("vaca");
+                }
+            }
+        }
+        else //invertebrado
+        {
+            if (word2.ToLower() == "inseto")
+            {
+                if (word3.ToLower() == "hematofago")
+                {
+                    Console.WriteLine("pulga");
+                }
+                else //herbivoro
+                {
+                    Console.WriteLine("lagarta");
+                }
+            }
+            else //anelideo
+            {
+                if (word3.ToLower() == "hematofago")
+                {
+                    Console.WriteLine("sanguessuga");
+                }
+                else //onivoro
+                {
+                    Console.WriteLine("minhoca");
+                }
+            }
+        }
+    }
+}
