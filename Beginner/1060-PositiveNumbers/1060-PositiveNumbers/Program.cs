f0f6b0d5c1cb64a5139757145bb97218d1b9b64b
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2018-06-18 20:22:18 -0300

    Answer to question 1060.

---
+++ Beginner/1060-PositiveNumbers/1060-PositiveNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int contador = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            double number = double.Parse(Console.ReadLine());
+            if (number > 0)
+            {
+                contador++;
+            }
+        }
+        Console.WriteLine("{0} valores positivos", contador);
+    }
+}
