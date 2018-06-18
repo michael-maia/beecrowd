538b7f50a787388a50c974addbbd98ef8176f43c
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2018-06-18 20:12:17 -0300

    Answer to question 1070.

---
+++ Beginner/1070-SixOddNumbers/1070-SixOddNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int contador = 0;
+        int i = x;
+        do
+        {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine(i);
+                contador++;
+            }
+            i++;
+        } while (contador != 6);
+    }
+}
