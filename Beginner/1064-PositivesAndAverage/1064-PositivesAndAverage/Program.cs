09b97f4fbb7e3beb9378ef498b4121ed06a132a3
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2018-06-18 20:21:30 -0300

    Answer to question 1064.

---
+++ Beginner/1064-PositivesAndAverage/1064-PositivesAndAverage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int contador = 0;
+        double soma = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            double number = double.Parse(Console.ReadLine());
+            if (number > 0)
+            {
+                soma += number;
+                contador++;
+            }
+        }
+        double media = soma / contador;
+        Console.WriteLine("{0} valores positivos\n" + media.ToString("0.0"), contador, media);
+    }
+}
