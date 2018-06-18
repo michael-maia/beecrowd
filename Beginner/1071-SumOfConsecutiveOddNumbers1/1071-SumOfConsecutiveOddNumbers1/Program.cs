db6354000081e96679e6d7bdceae20ad81f12f24
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2018-06-18 20:10:39 -0300

    Answer to question 1071.

---
+++ Beginner/1071-SumOfConsecutiveOddNumbers1/1071-SumOfConsecutiveOddNumbers1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int soma = 0;
+        if (n2 < n1)
+        {
+            int aux = n2;
+            n2 = n1;
+            n1 = aux;
+        }
+        for (int i = (n1 + 1); i < n2; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma += i;
+            }
+        }
+        Console.WriteLine(soma);
+    }
+}
