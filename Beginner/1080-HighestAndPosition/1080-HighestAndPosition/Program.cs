573b0e8f2a96e7d15f1cb055d6045501da786cdb
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2018-08-19 17:15:06 -0300

    Answer to question 1080

---
+++ Beginner/1080-HighestAndPosition/1080-HighestAndPosition/Program.cs
@@ -0,0 +1,30 @@
+﻿//Rextester.Program.Main is the entry point for your code. Don't change it.
+//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int maior = 0;
+            int posicao = 0;
+            int[] numeros = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                numeros[i] = int.Parse(Console.ReadLine());
+                if (numeros[i] > maior)
+                {
+                    maior = numeros[i];
+                    posicao = i;
+                }
+            }
+            Console.WriteLine("{0}\n{1}", maior, posicao + 1);
+        }
+    }
+}
