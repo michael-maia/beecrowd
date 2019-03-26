4c28048372067348f1b8c82d0501f848c212ded0
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2019-03-26 19:39:12 -0300

    Answer to question 1146.

---
+++ Beginner/1146-GrowingSequences/1146-GrowingSequences/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x == 0)
+            {
+                break;
+            }
+            for (int i = 1; i <= x; i++)
+            {
+                if (i < x)
+                {
+                    Console.Write(i + " ");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
