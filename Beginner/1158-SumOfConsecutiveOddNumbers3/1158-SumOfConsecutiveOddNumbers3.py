1a826b817e2f0fca67ace731d68d58f33689cbab
Michael Maia <32021280+michael-maia@users.noreply.github.com>
2019-09-12 18:38:50 -0300

    Add files via upload

--- Beginner/1158-SumOfConsecutiveOddNumbers3/1158-SumOfConsecutiveOddNumbers3.py
+++ Beginner/1158-SumOfConsecutiveOddNumbers3/1158-SumOfConsecutiveOddNumbers3.py
@@ -1,17 +1,16 @@
-N = int(input())
+N = int(input()) # Entrada do número de repetições
 
-soma = [0,0]
-
-for i in range(N):
-    X, Y = int(input()).split(" ")
-    cont = 0
-    j = X    
-    while cont < Y:
-        if j % 2 == 1:
-            soma[i] += j
+for i in range(N): # O primeiro loop é for pois sabemos o numero de vezes pra verificar
+    valores = input().split(" ")
+    X, Y = map(int,valores)
+    cont = 0 # Contador do numero de impares encontrados
+    j = X # A variavel j foi usada para nao alterar X diretamente
+    soma = 0
+    while cont < Y: # Enquanto j é menor que a quantidade de ímpares que queremos
+        if (j % 2) == 1 or (j % 2) == -1: # Para caso o número seja negativo o módulo será -1
+            soma += j
             j += 1
             cont += 1
         else:
             j += 1
-
-print(soma[0], soma[1])
+    print(soma)
