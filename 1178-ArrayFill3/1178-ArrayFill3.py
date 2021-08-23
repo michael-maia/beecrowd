bd08e7ca8104742eef7797b8218fd600fa4c7f66
michael-maia <michaelmdasilva@yahoo.com.br>
2021-08-22 21:20:30 -0300

    Question 1259 solved in Python

--- 1178-ArrayFill3/1178-ArrayFill3.py
+++
@@ -1,7 +0,0 @@
-# Question: https://www.urionlinejudge.com.br/judge/en/problems/view/1178
-
-X = float(input())
-Y = X
-for i in range(100):    
-    print("N[{}] = {:.4f}".format(i, Y))
-    Y /= 2
---
+++ Beginner/1259-Even_or_Odd/1259-Even_or_Odd.py
@@ -0,0 +1,30 @@
+# Declarando listas para incluir os números pares e ímpares
+list_odd = []
+list_even = []
+
+# Variável que indica quantos números vão ser avaliados
+N = int(input())
+
+# Caso o número N não esteja dentro do range específicado na questão
+if(N > 1 and N <= 100000):
+    # Loop para digitar os N números e verificar se é par/ímpar para colocar em sua respectiva lista
+    for i in range(N):
+        number = int(input())
+        if(number % 2 == 0):
+            list_even.append(number)
+        else:
+            list_odd.append(number)
+    # Ordenando as listas
+    # Ascendente
+    list_even.sort()
+    # Descendente
+    list_odd.sort(reverse = True)
+
+    # Printando as listas
+    def PrintList(numberList):
+        for value in numberList:
+            # LEMBRETE: print() já inclui nova linha
+            print(value)
+
+    PrintList(list_even)
+    PrintList(list_odd)
